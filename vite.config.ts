bb023f37d868cdcc331b2ceb7ef2d9cab8f70176
PepDev <gsrpjosephdoyle@gmail.com>
2026-06-19 21:04:51 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: "/sth1/",
+    base: "/sth4/",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
